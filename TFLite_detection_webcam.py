58639aa6c9724e3285c6a362db35ac8b4c5bba9d
jtristan <jtristan@pdx.edu>
2025-03-29 20:57:41 -0700

    tesing

--- TFLite_detection_webcam.py
+++ TFLite_detection_webcam.py
@@ -1,5 +1,5 @@
 ######## Webcam Object Detection Using Tensorflow-trained Classifier #########
-#V 2
+#V 3
 # Author: Evan Juras
 # Date: 10/27/19 created by author
 # Being used by the CCC robot ece410
